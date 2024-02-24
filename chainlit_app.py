053f08185d7f66138b3239731720ba5d3a03767d
Jorge García de Bustos <j.garciadebustos@godeltech.com>
2024-02-24 00:01:48 +0000

    Chainlit Web UI now using loguru

--- chainlit_app.py
+++ chainlit_app.py
@@ -6,6 +6,9 @@ Run the following on the command line:
 
 """
 
+import logging
+import inspect
+from loguru import logger
 
 from chainlit import (
     on_chat_start,
@@ -59,3 +62,27 @@ async def message_received(rcvd_message: Message) -> None:
         rcvd_message.content,
     )
     await Message(content=answer).send()
+
+
+class InterceptHandler(logging.Handler):
+    '''Class to intercept all logging with Loguru'''
+    def emit(self, record: logging.LogRecord) -> None:
+        # Get corresponding Loguru level if it exists.
+        level: str | int
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
